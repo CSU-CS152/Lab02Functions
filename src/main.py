025094a94c0c922648958a1bc50c0e1b7a722f9e
Will Daknis <daknis@rams.colostate.edu>
2022-05-31 10:23:44 -0600

    skeleton of main and functions

---
+++ src/main.py
@@ -0,0 +1,23 @@
+PI = 3.14
+
+def circleArea(radius):
+    return None
+
+def rectangleArea(length, width):
+    return None
+
+def triangleArea():
+    return None
+
+def trapezoidArea():
+    return None
+
+def printShapeInfo():
+    return None
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
