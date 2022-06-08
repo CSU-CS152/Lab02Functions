3ec55ba0c8ae27ee3397c13761ab045d19858a01
Will Daknis <68512532+willrd3@users.noreply.github.com>
2022-06-07 21:09:52 -0600

    delete main function

--- src/main.py
+++ src/main.py
@@ -16,25 +16,21 @@ def shapeInfo(type, area):
     return ''
 
 
-def main():
-    #feel free to uncomment any of the following code in main
-    #it is included here to help you test your program
+#feel free to uncomment any of the following code in main
+#it is included here to help you test your program
 
-    circle = circleArea(5)
-    # print('TESTING', circleArea(5))
-    # print('TESTING', shapeInfo('circle', circle))
+#circle = circleArea(5)
+# print('TESTING', circleArea(5))
+# print('TESTING', shapeInfo('circle', circle))
 
-    rectangle = rectangleArea(6,7)
-    # print('TESTING', rectangleArea(6,7))
-    # print('TESTING', shapeInfo('rectangle', rectangle))
+# rectangle = rectangleArea(6,7)
+# print('TESTING', rectangleArea(6,7))
+# print('TESTING', shapeInfo('rectangle', rectangle))
 
-    triangle = triangleArea(6,7)
-    # print('TESTING', triangleArea(6,7))
-    # print('TESTING', shapeInfo('triangle', triangle))
+# triangle = triangleArea(6,7)
+# print('TESTING', triangleArea(6,7))
+# print('TESTING', shapeInfo('triangle', triangle))
 
-    strapezoid = trapezoidArea(1,3,4)
-    # print('TESTING', trapezoidArea(1,3,4))
-    # print('TESTING', shapeInfo('trapezoid', trapezoid))
-
-if __name__ == "__main__":
-    main()
+# trapezoid = trapezoidArea(1,3,4)
+# print('TESTING', trapezoidArea(1,3,4))
+# print('TESTING', shapeInfo('trapezoid', trapezoid))
